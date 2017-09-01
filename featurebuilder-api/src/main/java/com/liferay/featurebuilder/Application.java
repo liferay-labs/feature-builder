0b603ce46d2d67d1b9be82467279afe2971b469f
Julio Camarero <julio.camarero@liferay.com>
2017-09-01 11:39:01 +0200

    amend con spring boot

--- featurebuilder-api/src/main/java/com/liferay/featurebuilder/Application.java
+++ featurebuilder-api/src/main/java/com/liferay/featurebuilder/Application.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -17,6 +17,10 @@ package com.liferay.featurebuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Julio Camarero
+ * @author Cristina Gonzalez
+ */
 @SpringBootApplication
 public class Application {
 
